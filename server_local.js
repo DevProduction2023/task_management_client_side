a3472612b08f625108d1886da34bb67c6566ceee
DevProduction2023 <shivcom2020@gmail.com>
2025-06-16 17:15:58 +0530

    Update server_local.js

--- server_local.js
+++ server_local.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: "https://task-management-client-5a04okinx-shivs-projects-0bdf001e.vercel.app", 
     credentials: true,
 }));
 
